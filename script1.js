4228b86dfabfb5a6f9a2c2cc61296fe7e7e0d34c
Margarita5761 <114502997+Margarita5761@users.noreply.github.com>
2023-03-15 17:14:51 +0700

    Добавление лабораторной работы
    Добавила 4 лабораторную работу из раздела 3

---
+++ script1.js
@@ -0,0 +1,102 @@
+// function minNum(a,b,c){
+//     var min=0;
+//     if(a<b && a<c){
+//         min=a;
+//     } else if(b<c && b<a){
+//         min=b;
+//     } else {
+//         min=c;
+//     }
+//     return [min]
+// } 
+// var array=[20, -6, 3];
+// console.log(minNum.apply(this, array));
+
+
+
+
+// let i=Number(prompt("Введите первое число"));
+// switch(i){
+//     case 1:
+//         console. log("31 days");
+//         break;
+//         case 2:
+//         console. log("28 days");
+//         break;
+//         case 3:
+//         console. log("31 days");
+//         break;
+//         case 4:
+//         console. log("30 days");
+//         break;
+//         case 5:
+//         console. log("31 days");
+//         break;
+//         case 6:
+//         console. log("30 days");
+//         break;
+//         case 7:
+//         console. log("31 days");
+//         break;
+//         case 8:
+//         console. log("31 days");
+//         break;
+//         case 9:
+//         console. log("30 days");
+//         break;
+//         case 10:
+//         console. log("31 days");
+//         break;
+//         case 11:
+//         console. log("30 days");
+//         break;
+//         case 12:
+//         console. log("31 days");
+//         break;
+//     default:
+//         console.log("default");
+// }
+
+
+
+
+// let num=5.9;
+// let exp=8;
+
+// for (r=1; exp>=r; r++){
+//     console.log(r);
+// }
+
+
+
+// function sayHello(name) {
+//     console.log(name)
+//   }
+  
+//   sayHello('Hello, world!')
+
+
+// function GetRandom (min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+//  let a=(GetRandom(1, 101));
+//  let b=2;
+//  console.log(Math.pow(a, b));
+
+
+// function echo (theline) {
+//     console.log(theline);
+// }
+// echo ("Привет, мир!");
+
+
+// function InvertString(b){
+//     let a="I love to cook and eat delicious";
+// let ar=a.split("");
+// for(let i=0; i<ar.length/2; i++){
+//     [ar[i], ar[ar.length-i-1]]=[ar[ar.length-i-1],ar[i]];
+//     console.log(b);
+//     b = ar.join(" ");
+// }
+// }
+// InvertString("b");
