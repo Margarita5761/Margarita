d5eb6c8b833545ea8bcc464bd4d91d65e6886e02
Margarita5761 <114502997+Margarita5761@users.noreply.github.com>
2023-03-15 17:11:56 +0700

    Добавление лабораторной работы
    Добавила 2 лабораторную работу из раздела 3

---
+++ result.js
@@ -0,0 +1,17 @@
+// let num=1;
+// num+=12;
+// num-=14;
+// num*=5;
+// num/=7;
+// num++;
+// num--;
+// alert(num);
+
+// console.log('между автомобилями $(distance - time * (speed1 + speed2)) км')
+
+// let a=prompt('edge');
+// console.log('Площадь грани куба: ${edge**2}');
+// console.log('Площадь поверхности куба: ${edge**2*6}');
+// console.log('Объем куба: ${edge**3}');
+
+
